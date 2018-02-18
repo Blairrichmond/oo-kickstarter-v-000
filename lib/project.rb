33722bbb8e348e74e3d44f8f92cf7427a9c168cd
Brent  Ransom <sadumedusa@gmail.com>
2018-02-18 12:51:30 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,11 +4,11 @@ class Project
   def initialize(title)
     @title = title
     @backers = []
-    backers.add_backer(self)
   end
 
   def add_backer(backer)
     @backers << backer
+    backers.add_backer(self)
   end
 
   def backers
